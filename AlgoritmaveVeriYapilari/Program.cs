08dda041c161344772e01b935ebfd1e7e8b6d0f7
denizokan.taspinar <denizokan.taspina@arcelik.com>
2024-07-29 03:50:46 +0300

    first commit

---
+++ AlgoritmaveVeriYapilari/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Giris
+{
+    class Giris
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 8; //işaretsiz byte, yalnızca pozitif değerler
+            sbyte num2 = -8; //işaretli byte
+
+            //byte 8 bit yer tutar: _ _ _ _ _ _ _ _ 
+            // soldaki bit 1 ise negatif 0 ise pozitif sayı tutar
+
+            Console.WriteLine("Byte min: {0} {1}", byte.MinValue, byte.MaxValue);
+
+            Console.ReadKey();
+        }
+    }
+}
